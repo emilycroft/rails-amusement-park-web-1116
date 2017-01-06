d78b8ebe05a2a14a5719989b9a3a0add1aad198b
emilycroft <emilyhcroft@gmail.com>
2017-01-06 13:21:50 -0500

    the real one

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'users#index', as: 'root'
   get '/users/new', to: 'users#new', as: 'signup'
-<<<<<<< HEAD
+
   get '/users/:id', to: 'users#show', as: 'user'
 
 
@@ -11,12 +11,6 @@ Rails.application.routes.draw do
   post '/attractions/:id', to: 'attractions#custom'
 
 
-=======
-  
-  get '/login', to: 'sessions#create', as: 'login'
-  get '/signin', to: 'sessions#new', as: "signin"
-  
->>>>>>> c98a09da926c4e6749935cd9db2f2616149efd9f
   resources :attractions
   resources :users
   resources :rides
